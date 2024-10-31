9f2af6375e9430201f8380667675ab0b7e71c2e1
Ramazan <ospanr4@gmail.com>
2024-10-31 14:53:02 +0500

    change users views on admin panel

--- online_market/carts/models.py
+++ online_market/carts/models.py
@@ -6,14 +6,19 @@ class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     items = models.ManyToManyField(Product, through='CartItem')
 
+    def __str__(self):
+        return f"Cart of {self.user.email}"  # Отображение email пользователя в админ-панели
+
     def get_total(self):
-        total = sum(item.get_cost() for item in self.cartitem_set.all())
-        return total
+        return sum(item.get_cost() for item in self.cartitem_set.all())
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"  # Отображение количества и названия товара
 
     def get_cost(self):
         return self.product.price * self.quantity
