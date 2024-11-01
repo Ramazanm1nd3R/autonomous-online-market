041e2b2051ba052688e5d4e548758ef04fb27586
Ramazan <ospanr4@gmail.com>
2024-11-01 21:12:50 +0500

    add swagger

--- online_market/online_market/settings.py
+++ online_market/online_market/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
-
+    'drf_yasg',
+    
     'user',  
     'common',
     'backend_api',
--- online_market/online_market/urls.py
+++ online_market/online_market/urls.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Настройка для Swagger и ReDoc
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Online Market API",
+        default_version="v1",
+        description="Документация API для проекта Online Market",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +32,8 @@ urlpatterns = [
     # JWT Authentication Endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Swagger и ReDoc интерфейсы
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
