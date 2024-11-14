92005747f0d970f627c6f57d17232dc4069df34c
Ramazan <ospanr4@gmail.com>
2024-11-14 19:35:40 +0500

    add password-change func, next add parameters to swagger, and email verify

--- online_market/user/urls.py
+++ online_market/user/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import UserRegistrationView, CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import ProtectedAPIView
+from .views import ProtectedAPIView, ChangePasswordView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('protected/', ProtectedAPIView.as_view(), name='protected-api'),
-
+    path('change-password/', ChangePasswordView.as_view(), name='change-password')
 ]
--- online_market/user/views.py
+++ online_market/user/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import UserRegistrationSerializer, CustomTokenObtainPairSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.hashers import check_password
 
 class UserRegistrationView(views.APIView):
     def post(self, request):
@@ -21,3 +22,28 @@ class ProtectedAPIView(APIView):
 
     def get(self, request):
         return Response({"message": "Это защищенный API, доступный только с токеном."})
+    
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data
+
+        # Получаем старый и новый пароли из запроса
+        old_password = data.get("old_password")
+        new_password = data.get("new_password")
+
+        # Проверяем старый пароль
+        if not check_password(old_password, user.password):
+            return Response({"error": "Старый пароль неверен"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Проверяем длину нового пароля
+        if len(new_password) < 8:
+            return Response({"error": "Новый пароль должен содержать не менее 8 символов"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Устанавливаем новый пароль
+        user.set_password(new_password)
+        user.save()
+
+        return Response({"success": "Пароль успешно изменен"}, status=status.HTTP_200_OK)
