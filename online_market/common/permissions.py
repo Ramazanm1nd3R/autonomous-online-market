b2331362332e09ed7ee0fe75d913c20a5ebad9a9
Adrian <adriangomis34@gmail.com>
2024-12-10 08:20:32 +0100

    Update permissions.py

--- online_market/common/permissions.py
+++ online_market/common/permissions.py
@@ -11,5 +11,5 @@ class IsAuthenticatedUser(permissions.BasePermission):
     """
     Allows access only to authenticated users.
     """
-    def has_permission(self, request, view):
+    def has_permission(self, request):
         return request.user and request.user.is_authenticated
